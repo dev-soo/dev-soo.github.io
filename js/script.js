582af8418ff496c487b4d581bd477f472e8316bc
srchoi <94441004+srchoi90@users.noreply.github.com>
2025-03-12 16:05:59 +0900

    portfolio list 작업

--- js/script.js
+++ js/script.js
@@ -1,40 +1,29 @@
 window.onload = function(){
     gsap.registerPlugin(ScrollTrigger);
 
-    gsap.utils.toArray('.lastest .lastest__item').forEach((selector) => {
-        gsap.timeline({
+    gsap.utils.toArray(".masked-image").forEach(item=>{
+        gsap.to(item,{
+            yPercent:-100,
+            ease:'none',
+            duration:0.5,
             scrollTrigger:{
-                trigger:selector,
-                start:'0% 100%',
-                end:'0% 0%',
-                scrub:1,
-                markers:true
+                trigger:item,
+                start:'bottom bottom',
+                end:'bottom top',
+                scrub:0.5
             }
         })
-        .to(selector, {transform:'rotateX(0) scale(1)',transformOrigin:'top', filter:'brightness(1)'},0)
     })
-
-    gsap.timeline({
-        scrollTrigger:{
-            trigger:'.test',
-            start:'0% 100%',
-            end:'100% 0%',
-            toggleClass:{targets:'main', className:'on'},
-            markers:true
-        }
-    })
-
-
-
+    
     const lenis = new Lenis({
-  // 추가된 부분
-  duration: 2,
-  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-});
+        // 추가된 부분
+        duration: 2,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    });
 
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
-requestAnimationFrame(raf);
+    function raf(time) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
 }
